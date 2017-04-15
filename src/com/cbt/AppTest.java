aead24bbd8ebe055bdfad38c6886e6038880243c
Salihz <salihzeki@gmail.com>
2017-04-15 12:40:59 -0400

    initial commit

---
+++ src/com/cbt/AppTest.java
@@ -0,0 +1,11 @@
+package com.cbt;
+
+//first plane Java project after GIT for Git  15 April 2017
+public class AppTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
